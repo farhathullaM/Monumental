0fde5dbdbefe31385090e8ab0703e6097dd8313b
farhathulla <farhathulla01@gmail.com>
2024-02-08 22:30:41 +0530

    burger menu

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,22 +4,49 @@ import logo_new from '../Assets/logo.png';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-    
-    //const [menu, setMenu] = useState("home");
+
+    let isNavOpen = false;
+function toggleNav(){
+    const navMenu = document.querySelector(".nav-menu")
+    const closeButton = document.querySelector(".menu-icons .fa-times")
+    const menuButton = document.querySelector(".menu-icons .fa-bars")
+    if(isNavOpen){
+
+        navMenu.classList.remove("nav-open")
+        isNavOpen = false
+        closeButton.style.display = "none"
+        menuButton.style.display = "block"
+    } 
+    else{
+        
+        navMenu.classList.add("nav-open")
+        isNavOpen = true
+        closeButton.style.display = "block"
+        menuButton.style.display = "none"
+    } 
+}
+
 return (
-<div className='navbar'>
-    <div className="nav-logo">
-        <img src={logo_new} alt="" />
-        <p>History</p>
-    </div>
-    <ul className='nav-menu'>
-        <li> <Link to='/login' style={{textDecoration: 'none'}}>Login </Link></li>
-        <li>Contact us</li>
-        <li>Explore</li>
-        <li>Blogs</li>
-    </ul>
-    <div className='menu'></div>
-</div>
+    <nav className='navbar'>
+        
+        <div className="nav-logo">
+            <img src={logo_new} alt="" />
+            <h1>History</h1>
+        </div>
+
+        <div className="menu-icons" onClick={toggleNav}>
+            <i className='fas fa-bars'></i>
+            <i className='fas fa-times'></i>
+        </div>
+
+        <ul className='nav-menu'>
+            <li> <Link to='./' className='navlink'> <i className="fa-solid fa-house-chimney"></i> Home </Link> </li>
+            <li> <Link to='/login' className='navlink'> <i className="fa-solid fa-right-to-bracket"></i> Login </Link> </li>
+            <li> <Link to='./' className='navlink'> <i className="fa-solid fa-address-card"></i> About us </Link> </li>
+            <li> <Link to='./' className='navlink'> <i class="fa-solid fa-blog"></i> Blogs </Link> </li>
+        </ul>
+
+    </nav>
 )
 }
 

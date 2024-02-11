0544c78990c2de6245253f928825d8e02f5db3ce
farhathulla <farhathulla01@gmail.com>
2024-02-11 21:59:50 +0530

    About head

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './About.css';
+import aboutimg from '../Assets/aboutusimg.jpg';
 
 const About = () => {
   return (
-    <div>
-      
+    <div className='about'>
+       <img src={aboutimg} alt="" />
+        <div className="home-txt">
+            <h1>About us</h1>
+            <a className='hide' href="/">Locality</a>
+        </div>
     </div>
   )
 }
 
-export default About
+export default About;

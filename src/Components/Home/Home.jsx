cf0541731bd86203624972c917c09f97aa4619e7
farhathulla <farhathulla01@gmail.com>
2024-02-21 14:38:37 +0530

    bck-gnd img

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import './Home.css';
-import coverimg from '../Assets/img1.jpg';
-import Popular from '../Popular/Popular';
+import tajmahacover from '../Assets/pexels-fuzail-ahmad-9208715.jpg'
+import Pagetwo from '../Pagetwo/Pagetwo';
+
 
 const Home = () => {
 return (
 <>
     <div className='home'>
-        <img src={coverimg} alt="" />
-        <div className="home-txt">
+        <div className="background-img">
+            <img src={tajmahacover} alt="" />
+        </div>
+        <div className="home-head">
             <h1>Let the World See</h1>
-            <p>Discover the wonders of everywhere</p>
-            <a className='show' href="/">Locality</a>
+            <p>Discover the Wonders</p>
+            <button className='discover-btn'>DISCOVER <i class="fa-solid fa-arrow-right"></i></button>
         </div>
     </div>
-    <Popular/>
+
+    <Pagetwo/>
 </>
 )
 }

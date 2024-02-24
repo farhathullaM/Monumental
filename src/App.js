05e1a40c063cb86cf2a7e0031dc5de2c10ce9a17
farhathulla <farhathulla01@gmail.com>
2024-02-24 19:16:52 +0530

    Path for showall page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import Footer from './Components/Footer/Footer';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Login from './Components/LoginSignup/Login';
 import SignUp from './Components/LoginSignup/Signup';
 import Home from './Components/Home/Home';
 import About from './Components/About/About';
+import Showall from './Components/Showall/Showall';
 
 function App() {
   return (
@@ -19,10 +19,10 @@ function App() {
           <Route path='/login' element={<Login/>}/>
           <Route path="/signup" exact element={<SignUp/>} />
           <Route path='/about' element={<About/>}/>
+          <Route path='/explore' element={<Showall/>}/>
       </Routes>
     
 
-      <Footer/>
       </BrowserRouter>
     </div>
   );

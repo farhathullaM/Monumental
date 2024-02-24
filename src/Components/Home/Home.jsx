b8d2c9b9b0db2405cc7bd080af29a57608516bcf
farhathulla <farhathulla01@gmail.com>
2024-02-24 19:13:04 +0530

    home page edits

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './Home.css';
 import tajmahacover from '../Assets/pexels-fuzail-ahmad-9208715.jpg'
 import Pagetwo from '../Pagetwo/Pagetwo';
+import Pagethree from '../Pagethree/Pagethree';
+import { Link } from 'react-router-dom';
 
 
 const Home = () => {
@@ -14,11 +16,14 @@ return (
         <div className="home-head">
             <h1>Let the World See</h1>
             <p>Discover the Wonders</p>
+            <Link to='./explore'>
             <button className='discover-btn'>DISCOVER <i class="fa-solid fa-arrow-right"></i></button>
+            </Link>
         </div>
     </div>
 
     <Pagetwo/>
+    <Pagethree/>
 </>
 )
 }
